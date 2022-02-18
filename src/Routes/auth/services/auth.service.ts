a9aa9be09e5f977ae7755533a368010a7c167e51
KartikBazzad <kartikbazzad@gmail.com>
2022-02-18 23:46:33 +0530

    upadted auth.controller.ts

--- src/Routes/auth/services/auth.service.ts
+++ src/Routes/auth/services/auth.service.ts
@@ -35,7 +35,7 @@ export class AuthService {
   async login(user: any) {
     const payload = { username: user.username, userId: user.userId };
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: await this.jwtService.sign(payload),
     };
   }
 }
