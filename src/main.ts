3ba5c8bdec188b9b2169a80029d87de12daa89cd
KartikBazzad <kartikbazzad@gmail.com>
2022-02-18 23:57:06 +0530

    upadted auth.controller.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.FRONTEND_URL,
+    credentials: true,
   });
   app.use(
     morgan(':method :url :status :res[content-length] - :response-time ms'),
